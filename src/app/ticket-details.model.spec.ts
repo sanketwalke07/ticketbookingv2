31adc8a7e8c7fe7ce80a46e2451ee6f5cb950b67
sanketwalke07 <sanketwalke07@gmail.com>
2023-06-05 04:00:33 +0530

    Auto-generated commit [ci skip]

--- src/app/ticket-details.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TicketDetails } from './ticket-details.model';
-
-describe('TicketDetails', () => {
-  it('should create an instance', () => {
-    expect(new TicketDetails()).toBeTruthy();
-  });
-});
